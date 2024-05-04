5a51f6aa8abff136edb845934c0cd8a7f64298e7
YounessBoumlik <younessboumlik1949@gmail.com>
2024-05-04 20:52:02 +0100

    update

--- GestionDeStock/src/application/ConnectToDB.java
+++ GestionDeStock/src/application/ConnectToDB.java
@@ -2,13 +2,15 @@ package application;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectToDB {
 	
 
-
-	public static void main(String[] args) {
+//	etablir une connection a la db.
+	public static Connection connectionDB() {
 
 		String url = "jdbc:mysql://localhost:3306/gestioncommande";
 		String user = "root";
@@ -28,7 +30,40 @@ public class ConnectToDB {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
+		System.out.println("Connextion reussite");
+		return connection;	
+	
 	}
+	
+//	prendre des donnes d'une collone d'un tableau preciser a partir d'une connection.
+	
+	public static void getData(Connection connection,String table,String column) {
+		
+		try {
+			Statement statement = connection.createStatement();
+			
+			String request = "SELECT * FROM " + table +" ;";
+			
+			
+			ResultSet result = statement.executeQuery(request);
+	
+			while (result.next()) {
+			    System.out.println(result.getString(column));
+			    // Process columnValue
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		Connection connexion = ConnectToDB.connectionDB();
+		getData(connexion,"facture","montant");
+	}
+	
 
 }
--- GestionDeStock/src/application/SampleController.java
+++ GestionDeStock/src/application/SampleController.java
@@ -1,5 +1,23 @@
 package application;
 
+import java.util.Date;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class SampleController {
 	
+    @FXML
+    private TableColumn<Facture, Date> datefacture;
+
+    @FXML
+    private TableColumn<Facture, Float> montant;
+
+    @FXML
+    private TableColumn<Facture, Integer> numerofacture;
+
+    @FXML
+    private TableView<Facture> tableventes;
+    
 }
