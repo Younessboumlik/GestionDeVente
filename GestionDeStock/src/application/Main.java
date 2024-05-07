5855a9b0d3a1a434d138741fb314d46f33719dcc
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-05-07 01:23:44 +0100

    last

--- GestionDeStock/src/application/Main.java
+++ GestionDeStock/src/application/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch(Exception e) { 
 			e.printStackTrace();
 		}
 	}
