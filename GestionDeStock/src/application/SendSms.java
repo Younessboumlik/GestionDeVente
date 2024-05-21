901cf99d082f6650ed7b316910f5e838730ef2f2
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-05-21 19:10:14 +0100

    ddddd

--- GestionDeStock/src/application/SendSms.java
+++ GestionDeStock/src/application/SendSms.java
@@ -5,8 +5,10 @@ import com.twilio.rest.api.v2010.account.Message;
 
 public class SendSms {
   // Find your Account Sid and Token at twilio.com/console
-  public static final String ACCOUNT_SID = "AC6ab3b891fac19c6aca18fb8624fa4550";
-  public static final String AUTH_TOKEN = "570f47e2ec833f38a8f1849eed3c48cb";
+//570f47e2ec833f38a8f1849eed3c48cb
+//AC6ab3b891fac19c6aca18fb8624fa4550 sid
+  public static final String ACCOUNT_SID = "";
+  public static final String AUTH_TOKEN = "";
 
   public static void smssend(String smsmessage) {
     Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
